b61077b6aab80d4bd2f136e9e8144458862be228
David Whipple <dwhipp88@gmail.com>
2023-02-14 23:13:18 -0700

    reverted to state before signInWithGoogleRedirect was implemented per instructions

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,26 +1,9 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
-
 import {
-    auth,
     signInWithGooglePopup,
-    signInWithGoogleRedirect,
     createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
-    useEffect(
-        () => async () => {
-            const response = await getRedirectResult(auth);
-            if (response) {
-                const userDocRef = await createUserDocumentFromAuth(
-                    response.user
-                );
-                console.log(userDocRef);
-            }
-        },
-        []
-    );
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
@@ -33,9 +16,7 @@ const SignIn = () => {
             <button onClick={logGoogleUser}>
                 Sign in with Google Popup
             </button>
-            <button onClick={signInWithGoogleRedirect}>
-                Sign in with Google Redirect
-            </button>
+            
         </div>
     );
 };
