22447829c465168d05af5c1c1c0e3f88fa6ea474
David Whipple <dwhipp88@gmail.com>
2023-02-16 18:48:11 -0700

    created sign in form component, refactored sign up into authentication component, added email and password authentication

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,6 +4,7 @@ import {
     signInWithPopup,
     GoogleAuthProvider,
     createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
 } from 'firebase/auth';
 import {
     getFirestore,
@@ -80,4 +81,12 @@ export const createAuthUserWithEmailAndPassword = async (
         email,
         password
     );
+};
+
+export const signInAuthUserWithEmailAndPassword = async (
+    email,
+    password
+) => {
+    if (!email || !password) return;
+    return await signInWithEmailAndPassword(auth, email, password);
 };
