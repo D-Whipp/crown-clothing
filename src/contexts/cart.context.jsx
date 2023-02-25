ee31dc4f7953c3ef6cf4d924a1f72bb07fe7053a
David Whipple <dwhipp88@gmail.com>
2023-02-24 21:00:55 -0700

    added arrow functionality for inc/dec item amount, added remove functionality

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -34,12 +34,20 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
     );
 };
 
+const clearCartItem = (cartItems, cartItemToClear) => {
+    return cartItems.filter(
+        (cartItem) => cartItem.id !== cartItemToClear.id
+    );
+};
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
     removeItemFromCart: () => {},
+    // clearItem
+    clearItemFromCart: () => {},
     cartCount: 0,
 });
 
@@ -64,11 +72,16 @@ export const CartProvider = ({ children }) => {
         setCartItems(removeCartItem(cartItems, cartItemToRemove));
     };
 
+    const clearItemFromCart = (cartItemToClear) => {
+        setCartItems(clearCartItem(cartItems, cartItemToClear));
+    };
+
     const value = {
         isCartOpen,
         setIsCartOpen,
         addItemToCart,
         removeItemFromCart,
+        clearItemFromCart,
         cartItems,
         cartCount,
     };
