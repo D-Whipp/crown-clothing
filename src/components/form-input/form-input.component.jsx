6f59bbec9d0d0b27b6a2e5a3bf64a1ae2af0c178
David Whipple <dwhipp88@gmail.com>
2023-03-02 16:32:38 -0700

    converted form input component scss styles into css in js styling

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,20 +1,24 @@
-import './form-input.styles.scss';
+import {
+    FormInputLabel,
+    Input,
+    Group,
+} from './form-input.styles.jsx';
 
 const FormInput = ({ label, ...otherProps }) => {
     return (
-        <div className="group">
-            <input className="form-input" {...otherProps} />
+        <Group>
+            <Input {...otherProps} />
 
             {label && (
-                <label
+                <FormInputLabel
                     className={`${
                         otherProps.value.length ? 'shrink' : ''
                     } form-input-label`}
                 >
                     {label}
-                </label>
+                </FormInputLabel>
             )}
-        </div>
+        </Group>
     );
 };
 
